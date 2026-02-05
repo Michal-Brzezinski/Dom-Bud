89992eb08b466c15f80b4f7c8a152b0f50232162
Michal Brzezinski <michal.brzezinski.off@gmail.com>
2026-02-06 00:06:04 +0100

    footer updated

--- app/views/partials/footer.php
+++ app/views/partials/footer.php
@@ -1,3 +1,15 @@
 <footer class="footer">
-  © 2026 DOM-BUD. Wszystkie prawa zastrzeżone.
+  <div class="footer__left"></div>
+
+  <div class="footer__center">
+    © 2026 DOM-BUD.
+  </div>
+
+  <div class="footer__right">
+    <span class="footer__label">Powered by</span>
+    <a href="https://github.com/Michal-Brzezinski" target="_blank" rel="noopener" class="footer__author">
+      <img src="/img/icons/github-logo.webp" alt="GitHub" class="footer__icon">
+      <span class="footer__name">Michał Brzeziński</span>
+    </a>
+  </div>
 </footer>
